969035a8aa86e00fbe0aa5001b972e7793940cde
japson1987 <276286171@qq.com>
2021-09-24 12:46:39 +0800

    Change fucntion name getCPHAddressFromPubKey to getHexAddressFromPubKey.

--- src/app/providers/wallet/wallet.service.ts
+++ src/app/providers/wallet/wallet.service.ts
@@ -3,12 +3,8 @@ import * as crypto from 'crypto';
 import * as nacl from 'tweetnacl';
 import * as bip39 from 'bip39';
 import * as RIPEMD160 from 'ripemd160';
-import * as util from 'ethereumjs-util';
 import * as jsSHA from 'jssha';
 import * as ed25519 from '@stablelib/ed25519';
-
-const TYPE_ED25519 = '01';
-const PUBKEY_PREFIX = '0120';//0x01   0x20 = 32 
 
 const PUBKEY_LENGTH = 64; // 32 bytes
 const SEED_LENGTH = 64; // 32 bytes
@@ -28,7 +24,7 @@ export class WalletService {
     let mnemonic = this.generateRandomMnemonic();
     let seed = this.generateSeed(mnemonic);
     let keyPair = this.generateKeyPair(seed);
-    let address = this.getCPHAddressFromPubKey(keyPair.publicKey);
+    let address = this.getHexAddressFromPubKey(keyPair.publicKey);
 
     return{
       address: address,
@@ -42,7 +38,7 @@ export class WalletService {
   fromMnemonic(mnemonic) {
     let seed = this.generateSeed(mnemonic);
     let keyPair = this.generateKeyPair(seed);
-    let address = this.getCPHAddressFromPubKey(keyPair.publicKey);
+    let address = this.getHexAddressFromPubKey(keyPair.publicKey);
 
     return {
       address: address,
@@ -55,40 +51,19 @@ export class WalletService {
 
   fromPrivateKey(privateKey) {
     let keyPair = this.generateKeyPairFromPrivate(privateKey);
-    let address = this.getCPHAddressFromPubKey(keyPair.publicKey);
+    let address = this.getHexAddressFromPubKey(keyPair.publicKey);
 
     return {
       address: address,
-      // mnemonic: mnemonic,
       path: "m/44'/60'/0'/0/0",
       privateKey: keyPair.privateKey,
       publicKey: keyPair.publicKey
     };
   }
-  /**
-   *  Create a new instance of this Wallet connected to provider.
-   */
-  // connect(provider: Provider): Wallet;
-  // getAddress(): Promise<string>;
-  // sign(transaction: TransactionRequest): Promise<string>;
-  // signMessage(message: Arrayish | string): Promise<string>;
-  // getBalance(blockTag?: BlockTag): Promise<BigNumber>;
-  // getTransactionCount(blockTag?: BlockTag): Promise<number>;
-  // sendTransaction(transaction: TransactionRequest): Promise<TransactionResponse>;
-  // encrypt(password: Arrayish | string, options?: any, progressCallback?: ProgressCallback): Promise<string>;
-  // /**
-  //  *  Static methods to create Wallet instances.
-  //  */
-  // static createRandom(options?: any): Wallet;
-  // static fromEncryptedJson(json: string, password: Arrayish, progressCallback?: ProgressCallback): Promise<Wallet>;
-  // static fromMnemonic(mnemonic: string, path?: string, wordlist?: Wordlist): Wallet;
-
-
   constructor() { }
 
   generateKeyPair(seed) {
     this._isHexString(seed, SEED_NAME, SEED_LENGTH);
-    // let buffString = this._hexStringToBytes(seed);
     let buffString = seed;
     let buffer = new Buffer(buffString, "hex")
     let keyPair = nacl.sign.keyPair.fromSeed(buffer);
@@ -116,21 +91,11 @@ export class WalletService {
     return hash.digest('hex').toUpperCase();
   }
 
-  getAddressFromPubKey(publicKey) {
+  getHexAddressFromPubKey(publicKey) {
     this._isHexString(publicKey, PUBKEY_NAME, PUBKEY_LENGTH);
-    let ripemd160 = new RIPEMD160();
-    let encodedPubKey = this._hexStringToBytes(TYPE_ED25519 + PUBKEY_PREFIX + publicKey);
-    var buffer = new Buffer(encodedPubKey);
-    return ripemd160.update(buffer).digest('hex').toUpperCase();
-  }
-
-  getCPHAddressFromPubKey(publicKey) {
-    this._isHexString(publicKey, PUBKEY_NAME, PUBKEY_LENGTH);
-
     let shaObj = new jsSHA("SHA3-256", "HEX");
     shaObj.update(publicKey);
     let hash = shaObj.getHash("HEX");
-
     let ripemd160 = new RIPEMD160();
     let encodedPubKey = this._hexStringToBytes(hash);
     var buffer = new Buffer(encodedPubKey);
@@ -141,7 +106,7 @@ export class WalletService {
   getAddressFromPrivKey(privateKey) {
     this._isHexString(privateKey, PRIVKEY_NAME, PRIVKEY_LENGTH);
     let publicKey = privateKey.substring(64, 128);
-    return this.getAddressFromPubKey(publicKey);
+    return this.getHexAddressFromPubKey(publicKey);
 
   }
 
@@ -168,7 +133,7 @@ export class WalletService {
     this._isHexString(publicKey, PUBKEY_NAME, PUBKEY_LENGTH);
     this._isHexString(address, ADDRESS_NAME, ADDRESS_LENGTH);
 
-    if (this.getCPHAddressFromPubKey(publicKey).toUpperCase() == address.toUpperCase())
+    if (this.getHexAddressFromPubKey(publicKey).toUpperCase() == address.toUpperCase())
       return true;
 
     return false;
